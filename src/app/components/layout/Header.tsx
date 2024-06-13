bcbc878bd75dc96e00538dd1b976c0cef5887f7d
Matthew <matthewridgway.deximo@gmail.com>
2024-06-13 23:58:27 +0800

    WIP Header component

---
+++ src/app/components/layout/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <div className="bg-teal w-full h-96">
+            <h1 className="text-white text-4xl font-bold">Restaurant Menu Simple CRUD</h1>
+        </div>
+    );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
         <html lang="en" data-theme="light">
             <body className={inter.className}>
                 <main className="flex flex-col w-full h-screen">
+                    <Header />
                     {children}
                 </main>
             </body>
