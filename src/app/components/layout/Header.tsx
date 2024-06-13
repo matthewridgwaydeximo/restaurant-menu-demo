50854aefbdce40a83610fc60d1dcbe59033ccb1c
Matthew <matthewridgway.deximo@gmail.com>
2024-06-14 00:48:07 +0800

    Add Header component

--- src/app/components/layout/Header.tsx
+++ src/app/components/layout/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-        <div className="bg-teal w-full h-96">
+        <div className="flex items-center bg-teal w-full h-16 p-8">
             <h1 className="text-white text-4xl font-bold">Restaurant Menu Simple CRUD</h1>
         </div>
     );
